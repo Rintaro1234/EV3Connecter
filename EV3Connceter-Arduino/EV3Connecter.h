02981bed1be8e5aa7742b4daa9d77802e1a22f37
Fox <rin.nagaya.00@gmail.com>
2021-08-10 13:33:59 +0900

    構成の見直し

--- EV3Connceter-Arduino/EV3Connecter.h
+++ EV3Connceter-Arduino/EV3Connecter.h
@@ -1,18 +1,12 @@
 #pragma once
 #include "Wire.h"
 
+#defien MyAddress 0x0A
+
 class EV3{
 private:
-    void *pfunc[256];
-    uint8_t getAddress;
-    uint8_t datas;
-    uint8_t *pdata;
-
-    void receiveEvent();
-    void requestEvent();
-    void check();
+    uint8_t funcAdd; // EV3からの実行命令
+    
 public:
-    void initI2CPort();
-    void sendItems(uint8_t *d, uint8_t num);
-    int setFunc(void *p, uint8_t address);
+
 };
